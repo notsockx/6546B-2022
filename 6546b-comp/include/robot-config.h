ad9a345932f17bbd701bed8e734f58238f538849
notsock <64080694+notsockx@users.noreply.github.com>
2022-04-13 21:49:58 +0800

    reformatted robot-config

--- 6546b-comp/include/robot-config.h
+++ 6546b-comp/include/robot-config.h
@@ -1,40 +1,39 @@
 using namespace vex;
-
-extern brain Brain;
-
 using signature = vision::signature;
 
-// VEXcode devices
-extern controller Controller1;
+// basic vex devices
+brain Brain;
+controller Controller1 = controller(primary);
+
+// motors
 extern motor twobar;
-extern signature vision7__SIG_1;
-extern signature vision7__SIG_2;
-extern signature vision7__SIG_3;
-extern signature vision7__SIG_4;
-extern signature vision7__SIG_5;
-extern signature vision7__SIG_6;
-extern signature vision7__SIG_7;
-extern vision vision7;
-extern rotation rotationRight;
-extern rotation rotationLeft;
-extern inertial inertial13;
-extern gps GPS18;
 extern motor intake;
-extern digital_out front;
-extern digital_out down_clamp;
-extern digital_out ring_clamp;
-extern digital_out ring_branch;
 extern motor Motor16;
 extern motor Motor17;
-extern rotation rotationBar;
 extern motor Motor14;
 extern motor Motor15;
 extern motor Motor9;
 extern motor Motor10;
 
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
- * This should be called at the start of your int main function.
- */
-void  vexcodeInit( void );
+// motor groups
+extern motor_group fourbar;
+extern motor_group leftside;
+extern motor_group rightside;
+extern motor_group drivemotors;
+
+// digital out
+extern digital_out front;
+extern digital_out down_clamp;
+extern digital_out ring_clamp;
+extern digital_out ring_branch;
+
+// sensors
+extern vision vision7;
+extern gps DriveGPS;
+extern rotation rotationRight;
+extern rotation rotationLeft;
+extern inertial inertial13;
+extern rotation rotationBary;
+
+// functions
+void vexcodeInit( void );
--- 6546b-comp/src/robot-config.cpp
+++ 6546b-comp/src/robot-config.cpp
@@ -1,44 +1,74 @@
-#include "vex.h"
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       robot-config.cpp                                          */
+/*    Author:       Emilio Orcullo                                            */
+/*    Created:      Mon April 11 2022                                         */
+/*    Description:  Robot configuration                                       */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
+#include "vex.h"
 using namespace vex;
 using signature = vision::signature;
 using code = vision::code;
 
-// A global instance of brain used for printing to the V5 Brain screen
-brain  Brain;
-
-// VEXcode device constructors
+// basic vex devices
+brain Brain;
 controller Controller1 = controller(primary);
+
+// motors
 motor twobar = motor(PORT6, ratio18_1, false);
-/*vex-vision-config:begin*/
-vision vision7 = vision (PORT7, 50);
-/*vex-vision-config:end*/
-rotation rotationRight = rotation(PORT8, false);
-rotation rotationLeft = rotation(PORT12, true);
-inertial inertial13 = inertial(PORT13);
-gps GPS18 = gps(PORT18, 0.00, 0.00, mm, 180);
 motor intake = motor(PORT19, ratio6_1, false);
-digital_out front = digital_out(Brain.ThreeWirePort.B);
-digital_out down_clamp = digital_out(Brain.ThreeWirePort.C);
-digital_out ring_clamp = digital_out(Brain.ThreeWirePort.D);
-digital_out ring_branch = digital_out(Brain.ThreeWirePort.A);
 motor Motor16 = motor(PORT16, ratio36_1, true);
 motor Motor17 = motor(PORT17, ratio36_1, false);
-rotation rotationBar = rotation(PORT11, false);
 motor Motor14 = motor(PORT14, ratio6_1, true);
 motor Motor15 = motor(PORT15, ratio6_1, true);
 motor Motor9 = motor(PORT9, ratio6_1, false);
 motor Motor10 = motor(PORT10, ratio6_1, false);
 
-// VEXcode generated functions
+// motor groups
+motor_group fourbar(Motor16, Motor17);
+motor_group leftside(Motor14, Motor15);
+motor_group rightside(Motor9, Motor10);
+motor_group drivemotors(Motor9, Motor10, Motor14, Motor15);
+
+// digital out
+digital_out front = digital_out(Brain.ThreeWirePort.B);
+digital_out down_clamp = digital_out(Brain.ThreeWirePort.C);
+digital_out ring_clamp = digital_out(Brain.ThreeWirePort.D);
+digital_out ring_branch = digital_out(Brain.ThreeWirePort.A);
+
+// sensors
+vision vision7 = vision (PORT7, 50);
+gps DriveGPS = gps(PORT18, 0.00, 0.00, mm, 180);
+rotation rotationRight = rotation(PORT8, false);
+rotation rotationLeft = rotation(PORT12, true);
+inertial inertial13 = inertial(PORT13);
+rotation rotationBar = rotation(PORT11, false);
+
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
 
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
- * This should be called at the start of your int main function.
- */
+// init and setup
 void vexcodeInit( void ) {
-  // nothing to initialize
+  // start setup
+  Brain.Screen.clearScreen();
+  Brain.Screen.print("Device initialization...");
+  Brain.Screen.setCursor(2, 1);
+
+  // calibrate the GPS
+  wait(200, msec);
+  DriveGPS.calibrate();
+  Brain.Screen.print("Calibrating GPS for Drivetrain");
+
+  // wait for the GPS calibration process to finish
+  while (DriveGPS.isCalibrating()) {
+    wait(25, msec);
+  }
+
+  // reset the screen now that the calibration is complete
+  Brain.Screen.clearScreen();
+  Brain.Screen.setCursor(1,1);
+  wait(50, msec);
+  Brain.Screen.clearScreen();
 }
